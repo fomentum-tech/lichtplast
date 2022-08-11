eb874eb2bdbb523a9eb9dd5b3e040b4420633fc8
Davyson Santos <davyson.william02@hotmail.com>
2022-08-11 12:39:12 -0300

    update: removed link from baloo font

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,16 +43,6 @@ export default function Home() {
     <>
       <Head>
         <title>Lichtplast</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@600&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <MenuMobile />
